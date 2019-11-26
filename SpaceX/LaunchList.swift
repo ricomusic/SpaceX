cccb513a04c8c3fe2441c2ac86c845ab12b5d627
Ricardo Gonzalez <ricardo76@me.com>
2019-11-26 19:48:54 +0100

    Initial Commit

---
+++ SpaceX/LaunchList.swift
@@ -0,0 +1,15 @@
+// SpaceX - LaunchList.swift Created 2019-11-26
+
+import SwiftUI
+
+struct LaunchList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchList_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchList()
+    }
+}
