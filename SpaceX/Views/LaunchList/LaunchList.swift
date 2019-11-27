5697fa6beedfa5641f0c5ff992a5c88961f5a37b
Ricardo Gonzalez <ricardo76@me.com>
2019-11-27 05:48:28 +0100

    Add launch list

---
+++ SpaceX/Views/LaunchList/LaunchList.swift
@@ -0,0 +1,26 @@
+// SpaceX - LaunchList.swift Created 2019-11-26
+
+import SwiftUI
+
+struct LaunchList: View {
+    @ObservedObject var viewModel: LaunchListViewmodel
+
+    init() {
+        self.viewModel = LaunchListViewmodel()
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.launches, id: \.flightNumber) { launch in
+                    LaunchListRow(launch)
+                }
+            }
+        }
+    }
+}
+
+struct LaunchListPreviews: PreviewProvider {
+    static var previews: some View {
+        LaunchList()
+    }
+}
